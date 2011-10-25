d84aaeeef55dcc16e7e5eaf35c75fe0daa7c02a2
Robert Avery <daranable@gmail.com>
2011-10-24 23:40:13 -0500

    Removed output and wirelinks from clientside wire.

--- lua/starfall2/libs_cl/wire.lua
+++ lua/starfall2/libs_cl/wire.lua
@@ -6,25 +6,6 @@ SF.Libraries.Register("wire",wire_library)
 SF.Wire = {}
 SF.Wire.Library = wire_library
 
-local wirelink_metatable = {}
-local wlwrap, wlunwrap = SF.CreateWrapper(wirelink_metatable)
-
----
--- @class table
--- @name SF.Wire.WlMetatable
-SF.Wire.WlMetatable = wirelink_metatable
-
----
--- @class function
--- @name SF.Wire.WlWrap
--- @param wirelink
-SF.Wire.WlWrap = wlwrap
-
----
--- @class function
--- @name SF.Wire.WlUnwrap
--- @param wrapped
-SF.Wire.WlUnwrap = wlunwrap
 
 -- ------------------------- Internal Library ------------------------- --
 
@@ -36,26 +17,6 @@ local inputConverters =
 	VECTOR = identity,
 	ANGLE = identity,
 	WIRELINK = function(wl) return wlwrap(wl) end,
-}
-
-local outputConverters =
-{
-	NORMAL = function(data)
-		SF.CheckType(data,"number",1)
-		return data
-	end,
-	STRING = function(data)
-		SF.CheckType(data,"string",1)
-		return data
-	end,
-	VECTOR = function(data)
-		SF.CheckType(data,"Vector",1)
-		return data
-	end,
-	ANGLE = function(data)
-		SF.CheckType(data,"Angle",1)
-		return data
-	end
 }
 
 local WireLib = {}
@@ -180,125 +141,6 @@ function wire_library.createInputs(names, types)
 	WireLib.AdjustSpecialInputs(ent,names,types)
 end
 
---- Creates/Modifies wire outputs. All wire ports must begin with an uppercase
--- letter and contain only alphabetical characters.
--- @param names An array of output names. May be modified by the function.
--- @param types An array of output types. May be modified by the function.
-function wire_library.createOutputs(names, types)
-
-end
-
---- Returns the wirelink representing this entity. This is how you read inputs.
-function wire_library.self()
-	local ent = SF.instance.data.entity
-	if not ent then error("No entity",2) end
-	return wlwrap(ent)
-end
-
--- ------------------------- Wirelink ------------------------- --
-
---- Retrieves an output. Returns nil if the input doesn't exist.
-wirelink_metatable.__index = function(self,k)
-	SF.CheckType(self,wirelink_metatable)
-	if wirelink_metatable[k] and k:sub(1,2) ~= "__" then return wirelink_metatable[k]
-	else
-		local wl = wlunwrap(self)
-		if not wl or not wl:IsValid() or not wl.extended then return end -- TODO: What is wl.extended?
-		
-		if type(k) == "number" then
-			if not wl.ReadCell then return nil
-			else return wl:ReadCell(k) end
-		else
-			local output = wl.Outputs[k]
-			if not output or not inputConverters[output.Type] then return end
-			return inputConverters[output.Type](output.Value)
-		end
-	end
-end
-
---- Writes to an input.
-wirelink_metatable.__newindex = function(self,k,v)
-	SF.CheckType(self,wirelink_metatable)
-	local wl = wlunwrap(self)
-	if not wl or not wl:IsValid() or not wl.extended then return end -- TODO: What is wl.extended?
-	if type(k) == "number" then
-		SF.CheckType(v,"number")
-		if not wl.WriteCell then return end
-		
-	else
-		local input = wl.Inputs[k]
-		if not input or not outputConverters[input.Type] then return end
-		Wire_TriggerOutput(wl,input.Name,outputConverters[input.Type](v))
-	end
-end
-
-SF.Typedef("Wirelink",wirelink_metatable)
-
---- Checks if a wirelink is valid. (ie. doesn't point to an invalid entity)
-function wirelink_metatable:isValid()
-	SF.CheckType(self,wirelink_metatable)
-	return wlunwrap(self) and true or false
-end
-
---- Returns the type of input name, or nil if it doesn't exist
-function wirelink_metatable:inputType(name)
-	SF.CheckType(self,wirelink_metatable)
-	local wl = wlunwrap(self)
-	if not wl then return end
-	local input = wl.Inputs[name]
-	return input and input.Type
-end
-
---- Returns the type of output name, or nil if it doesn't exist
-function wirelink_metatable:outputType(name)
-	SF.CheckType(self,wirelink_metatable)
-	local wl = wlunwrap(self)
-	if not wl then return end
-	local output = wl.Outputs[name]
-	return output and output.Type
-end
-
---- Returns the entity that the wirelink represents
-function wirelink_metatable:entity()
-	SF.CheckType(self,wirelink_metatable)
-	return SF.Entities.Wrap(wlunwrap(self))
-end
-
---- Returns a table of all of the wirelink's inputs
-function wirelink_metatable:inputs()
-	SF.CheckType(self,wirelink_metatable)
-	local wl = wlunwrap(self)
-	if not wl then return nil end
-	local inputs = {}
-	for i=1,#wl.Inputs do
-		inputs[i] = wl.Inputs[i].Name
-	end
-	return inputs
-end
-
---- Returns a table of all of the wirelink's outputs
-function wirelink_metatable:outputs()
-	SF.CheckType(self,wirelink_metatable)
-	local wl = wlunwrap(self)
-	if not wl then return nil end
-	local outputs = {}
-	for i=1,#wl.Outputs do
-		outputs[i] = wl.Outputs[i].Name
-	end
-	return outputs
-end
-
---- Checks if an input is wired.
-function wirelink_metatable:isWired(name)
-	SF.CheckType(self,wirelink_metatable)
-	SF.CheckType(name,"string")
-	local wl = wlunwrap(self)
-	if not wl then return nil end
-	local input = wl.Inputs[name]
-	if input and input.Src and input.Src:IsValid() then return true
-	else return false end
-end
-
 -- ------------------------- Easy-Access Metatable ------------------------- --
 -- TODO: Replace with a wirelink to instance.data.entity
 local wire_ports_metatable = {}
@@ -322,16 +164,7 @@ function wire_ports_metatable:__index(name)
 end
 
 function wire_ports_metatable:__newindex(name,value)
-	SF.CheckType(name,"string")
-
-	local instance = SF.instance
-	local ent = instance.data.entity
-	if not ent then error("No entity",2) end
-
-	local output = ent.Outputs[name]
-	if not output then return end
-	
-	Wire_TriggerOutput(ent, name, outputSerializers[output.Type](value))
+	error( "Can't set outputs on client side" )
 end
 
 SF.Typedef("Ports",wire_ports_metatable)
